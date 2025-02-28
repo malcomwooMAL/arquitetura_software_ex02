df88892fca0cb784c776411d89b43b771312470e
malcomwoo <tadashimal@gmail.com>
2025-02-28 19:14:51 -0300

    exercicio de arquitetura de software inicio

--- src/Pedido.java
+++ src/Pedido.java
@@ -3,28 +3,88 @@
 //por completo
 public class Pedido implements Invoice{
 
+    //instanciacao dos Modulos
+    private Notificator notificator;
+    private PaymentMethod paymentM;
+
+    public void setNotificator(Notificator notificator) {
+        this.notificator = notificator;
+    }
+
+    public void setPaymentM(PaymentMethod paymentM) {
+        this.paymentM = paymentM;
+    }
+
+    public Notificator getNotificator() {
+        return notificator;
+    }
+
+    public PaymentMethod getPaymentM() {
+        return paymentM;
+    }
+
+    //atributos de instancia preliminares
     private String produtoId; //cada produto possui um numero de identificacao
     private double preco; //preco unitario
     private int quantidade;
     private String clienteId;
-    private String discountType;
-    private String notifier; // identificacao da localizacao do produto
+    private int discountType;
+    private int notifier; // identificacao da localizacao do produto
+    private int paymentMethod;
     private double subtotal;
-    private String formaPagamento;
 
-    public Pedido(String produtoId, double preco, int quantidade, String clienteId, String discountType, String notifier, String formaPagamento) {
+    //atributo de instancia para o calculo do preco final
+    private double precoFinal;
+
+    public Pedido(String produtoId, double preco, int quantidade, String clienteId, int discountType, int notifier, int paymentMethod) {
         this.produtoId = produtoId;
         this.preco = preco;
         this.quantidade = quantidade;
         this.clienteId = clienteId;
         this.discountType = discountType;
         this.notifier = notifier;
-        this.subtotal = preco * quantidade;
-        this.formaPagamento = formaPagamento;
+        this.paymentMethod = paymentMethod;
+        this.subtotal = this.preco * this.quantidade;
+    }
+
+    public String getProdutoId() {
+        return produtoId;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public String getClienteId() {
+        return clienteId;
+    }
+
+    public int getDiscountType() {
+        return discountType;
+    }
+
+    public int getNotifier() {
+        return notifier;
+    }
+
+    public int getPaymentMethod() {
+        return paymentMethod;
     }
 
     public double getSubtotal() {
         return subtotal;
+    }
+
+    public double getGetPrecoFinal() {
+        return precoFinal;
+    }
+
+    public void setPrecoFinal(double precoFinal){
+        this.precoFinal = precoFinal;
     }
 
     @Override
@@ -36,6 +96,7 @@ public class Pedido implements Invoice{
                 ", clienteId='" + clienteId + '\'' +
                 ", discountType='" + discountType + '\'' +
                 ", notifier='" + notifier + '\'' +
+                ", paymentMethod='" + paymentMethod + '\'' +
                 ", subtotal=" + subtotal +
                 ".\n";
     }
